613415c61c4dd37233228634c0d62de78e5221e5
Márton Bán <marton.bn@gmail.com>
2021-04-13 01:42:32 +0200

    AddLapTime/AddUser UI

--- src/frontend/AddLapTime.java
+++ src/frontend/AddLapTime.java
@@ -3,8 +3,26 @@ package frontend;
 import java.awt.EventQueue;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
 
 public class AddLapTime extends JDialog {
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+	private JTextField textField_9;
+	private JTextField textField_10;
+	private JTextField textField_11;
 
 	/**
 	 * Launch the application.
@@ -27,8 +45,152 @@ public class AddLapTime extends JDialog {
 	 * Create the dialog.
 	 */
 	public AddLapTime() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 423, 423);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Add New Lap:");
+		lblNewLabel.setFont(new Font("Mongolian Baiti", Font.BOLD, 19));
+		lblNewLabel.setBounds(10, 10, 130, 22);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblTrack = new JLabel("Track:");
+		lblTrack.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblTrack.setBounds(10, 49, 53, 18);
+		getContentPane().add(lblTrack);
+		
+		textField = new JTextField();
+		textField.setBounds(73, 49, 40, 19);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblSector = new JLabel("Sector 1");
+		lblSector.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblSector.setBounds(10, 77, 53, 18);
+		getContentPane().add(lblSector);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(10, 105, 29, 19);
+		getContentPane().add(textField_1);
+		
+		JLabel lblSector_1 = new JLabel(":");
+		lblSector_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblSector_1.setBounds(49, 105, 10, 18);
+		getContentPane().add(lblSector_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(59, 105, 53, 19);
+		getContentPane().add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(133, 105, 80, 19);
+		getContentPane().add(textField_3);
+		
+		JLabel lblSector_1_1 = new JLabel(":");
+		lblSector_1_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblSector_1_1.setBounds(123, 105, 10, 18);
+		getContentPane().add(lblSector_1_1);
+		
+		JLabel lblSector_4 = new JLabel("Sector 2:");
+		lblSector_4.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblSector_4.setBounds(10, 143, 72, 18);
+		getContentPane().add(lblSector_4);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(10, 171, 29, 19);
+		getContentPane().add(textField_4);
+		
+		JLabel lblSector_1_2 = new JLabel(":");
+		lblSector_1_2.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblSector_1_2.setBounds(49, 171, 10, 18);
+		getContentPane().add(lblSector_1_2);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(59, 171, 53, 19);
+		getContentPane().add(textField_5);
+		
+		JLabel lblSector_1_1_1 = new JLabel(":");
+		lblSector_1_1_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblSector_1_1_1.setBounds(123, 171, 10, 18);
+		getContentPane().add(lblSector_1_1_1);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(133, 171, 80, 19);
+		getContentPane().add(textField_6);
+		
+		JLabel lblSector_3 = new JLabel("Sector 3:");
+		lblSector_3.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblSector_3.setBounds(10, 208, 72, 18);
+		getContentPane().add(lblSector_3);
+		
+		textField_7 = new JTextField();
+		textField_7.setColumns(10);
+		textField_7.setBounds(10, 236, 29, 19);
+		getContentPane().add(textField_7);
+		
+		JLabel lblSector_1_3 = new JLabel(":");
+		lblSector_1_3.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblSector_1_3.setBounds(49, 236, 10, 18);
+		getContentPane().add(lblSector_1_3);
+		
+		textField_8 = new JTextField();
+		textField_8.setColumns(10);
+		textField_8.setBounds(59, 236, 53, 19);
+		getContentPane().add(textField_8);
+		
+		JLabel lblSector_1_1_2 = new JLabel(":");
+		lblSector_1_1_2.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblSector_1_1_2.setBounds(123, 236, 10, 18);
+		getContentPane().add(lblSector_1_1_2);
+		
+		textField_9 = new JTextField();
+		textField_9.setColumns(10);
+		textField_9.setBounds(133, 236, 80, 19);
+		getContentPane().add(textField_9);
+		
+		textField_10 = new JTextField();
+		textField_10.setColumns(10);
+		textField_10.setBounds(241, 49, 40, 19);
+		getContentPane().add(textField_10);
+		
+		JLabel lblDriverId = new JLabel("Driver ID:");
+		lblDriverId.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblDriverId.setBounds(146, 49, 85, 18);
+		getContentPane().add(lblDriverId);
+		
+		JLabel lblCompount = new JLabel("Compount:");
+		lblCompount.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblCompount.setBounds(190, 274, 85, 18);
+		getContentPane().add(lblCompount);
+		
+		JLabel lblSetupId = new JLabel("SetUp ID:");
+		lblSetupId.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblSetupId.setBounds(10, 275, 80, 18);
+		getContentPane().add(lblSetupId);
+		
+		textField_11 = new JTextField();
+		textField_11.setColumns(10);
+		textField_11.setBounds(100, 274, 40, 19);
+		getContentPane().add(textField_11);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(271, 273, 72, 21);
+		getContentPane().add(comboBox);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setFont(new Font("Tahoma", Font.ITALIC, 9));
+		btnNewButton.setBounds(221, 355, 85, 21);
+		getContentPane().add(btnNewButton);
+		
+		JButton btnAddLap = new JButton("Add Lap");
+		btnAddLap.setFont(new Font("Tahoma", Font.BOLD, 10));
+		btnAddLap.setBounds(316, 355, 85, 21);
+		getContentPane().add(btnAddLap);
 
 	}
-
 }
--- src/frontend/AddUser.java
+++ src/frontend/AddUser.java
@@ -3,8 +3,19 @@ package frontend;
 import java.awt.EventQueue;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
 
 public class AddUser extends JDialog {
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JPasswordField passwordField;
+	private JPasswordField passwordField_1;
 
 	/**
 	 * Launch the application.
@@ -29,7 +40,79 @@ public class AddUser extends JDialog {
 	public AddUser() {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Add User:");
+		lblNewLabel.setFont(new Font("Mongolian Baiti", Font.BOLD, 18));
+		lblNewLabel.setBounds(10, 10, 137, 25);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Name:");
+		lblNewLabel_1_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblNewLabel_1_1.setBounds(10, 53, 45, 13);
+		getContentPane().add(lblNewLabel_1_1);
+		
+		textField = new JTextField();
+		textField.setBounds(126, 53, 137, 19);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(126, 134, 56, 19);
+		getContentPane().add(textField_1);
+		
+		JLabel lblNewLabel_1_1_1 = new JLabel("Salary:");
+		lblNewLabel_1_1_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblNewLabel_1_1_1.setBounds(10, 134, 45, 13);
+		getContentPane().add(lblNewLabel_1_1_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(126, 192, 96, 19);
+		getContentPane().add(textField_2);
+		
+		JLabel lblNewLabel_1_1_1_1 = new JLabel("Birthday:");
+		lblNewLabel_1_1_1_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblNewLabel_1_1_1_1.setBounds(10, 192, 96, 13);
+		getContentPane().add(lblNewLabel_1_1_1_1);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(126, 163, 137, 19);
+		getContentPane().add(textField_3);
+		
+		JLabel lblNewLabel_1_1_2 = new JLabel("Position:");
+		lblNewLabel_1_1_2.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblNewLabel_1_1_2.setBounds(10, 163, 96, 13);
+		getContentPane().add(lblNewLabel_1_1_2);
+		
+		JLabel lblNewLabel_1_1_1_1_1 = new JLabel("Password Agin");
+		lblNewLabel_1_1_1_1_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblNewLabel_1_1_1_1_1.setBounds(10, 108, 108, 13);
+		getContentPane().add(lblNewLabel_1_1_1_1_1);
+		
+		JLabel lblNewLabel_1_1_2_1 = new JLabel("Password:");
+		lblNewLabel_1_1_2_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
+		lblNewLabel_1_1_2_1.setBounds(10, 79, 67, 13);
+		getContentPane().add(lblNewLabel_1_1_2_1);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(126, 79, 108, 19);
+		getContentPane().add(passwordField);
+		
+		passwordField_1 = new JPasswordField();
+		passwordField_1.setBounds(126, 108, 108, 19);
+		getContentPane().add(passwordField_1);
+		
+		JButton btnNewButton = new JButton("Back");
+		btnNewButton.setFont(new Font("Tahoma", Font.ITALIC, 9));
+		btnNewButton.setBounds(246, 235, 85, 21);
+		getContentPane().add(btnNewButton);
+		
+		JButton btnAddUser = new JButton("Add User");
+		btnAddUser.setFont(new Font("Tahoma", Font.BOLD, 10));
+		btnAddUser.setBounds(341, 235, 85, 21);
+		getContentPane().add(btnAddUser);
 
 	}
-
 }
